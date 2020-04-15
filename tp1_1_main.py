a6f259b67d1b71b6e21978b36c7adbf14d9a4a2c
Marcelo Castellano <marce.geek22@gmail.com>
2020-04-15 14:43:10 -0300

    Fix estructura de paquetes de TPs

---
+++ tp1_1_main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# script para correr el código directamente con `python3 tp1_1_main.py`
+from tps.tp1 import tp1_ruleta as tp_ruleta
+
+if __name__ == '__main__':
+    tp_ruleta.main()
--- tp1_main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# script para correr el código directamente con `python3 tp1_main.py`
-from tps import tp1_ruleta
-
-if __name__ == '__main__':
-    tp1_ruleta.main()
